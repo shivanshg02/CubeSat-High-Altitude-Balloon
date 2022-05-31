0540825db9bf1d28c654a45de911a45a10adca1c
magpie31415 <findmagpie@gmail.com>
2022-05-30 19:56:43 -0700

    py scripts

--- rtc_ds1307_setup.py
+++
@@ -1,8 +0,0 @@
-import time
-import board
-import adafruit_ds1307
-
-i2c = board.I2C()  # uses board.SCL and board.SDA
-rtc = adafruit_ds1307.DS1307(i2c)
-t = time.struct_time((2022, 5, 31, 23, 59, 59, 1, -1, -1))
-rtc.datetime = t
