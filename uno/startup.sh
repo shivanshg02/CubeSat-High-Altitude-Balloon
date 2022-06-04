5e0c7b2df62f61baa646c95242f47ad1bf2e10c1
Shivansh Gupta <sggupta@caltech.edu>
2022-06-04 00:25:42 -0700

    ree

--- uno/startup.sh
+++ uno/startup.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
+
+/bin/sleep 30
+
+cd /home/uno/Desktop/CubeSat-High-Altitude-Balloon/
+
 python3 /home/uno/Desktop/CubeSat-High-Altitude-Balloon/uno/logger.py & 
 python3 /home/uno/Desktop/CubeSat-High-Altitude-Balloon/uno/sender.py & 
 python3 /home/uno/Desktop/CubeSat-High-Altitude-Balloon/uno/recordvideo.py
+
+/bin/sleep 60
+
+sudo reboot
